e95308b6ded195789ea409cc0f3c3c38c4fd1541
ciupa.kamil <194928@edu.p.lodz.pl>
2017-05-16 12:08:36 +0200

    first test implemented green

---
+++ src/test/java/edu/iis/mto/serverloadbalancer/CurrentLoadPercentageMatcher.java
@@ -0,0 +1,26 @@
+package edu.iis.mto.serverloadbalancer;
+
+import org.hamcrest.Description;
+import org.hamcrest.Matcher;
+import org.hamcrest.TypeSafeMatcher;
+
+public class CurrentLoadPercentageMatcher extends TypeSafeMatcher<Server> {
+
+	private double expectedLoadPercentage;
+
+	public CurrentLoadPercentageMatcher(double expectedLoadPercentage) {
+		this.expectedLoadPercentage = expectedLoadPercentage;
+	}
+
+	public void describeTo(Description description) {
+		description.appendText("a server with load percentage of ").appendValue(expectedLoadPercentage);
+	}
+
+	@Override
+	protected boolean matchesSafely(Server item) {
+		return expectedLoadPercentage == item.currentLoadPercentage || 
+				Math.abs(expectedLoadPercentage - item.currentLoadPercentage) <= 0.01d;
+	}
+	
+
+}
--- src/test/java/edu/iis/mto/serverloadbalancer/ServerLoadBalancerTest.java
+++ src/test/java/edu/iis/mto/serverloadbalancer/ServerLoadBalancerTest.java
@@ -4,6 +4,7 @@ package edu.iis.mto.serverloadbalancer;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
 
+import org.hamcrest.Matcher;
 import org.junit.Test;
 
 public class ServerLoadBalancerTest {
@@ -11,6 +12,40 @@ public class ServerLoadBalancerTest {
 	public void itCompiles() {
 		assertThat(true, equalTo(true));
 	}
+	
+	@Test
+	public void balancingServer_NoVm_serverEmpty() {
+		
+		Server theServer = a(server().withCapacity(1));
+		
+		balancing(aServersListwith(theServer), aVmsListEmpty());
+		
+		assertThat(theServer, hasCurrentLoadOf(0.0d));
+		
+	}
 
+	private Matcher<? super Server> hasCurrentLoadOf(double expectedLoadPercentage) {
+		return new CurrentLoadPercentageMatcher(expectedLoadPercentage);
+	}
+
+	private void balancing(Server[] servers, Vm[] vms) {
+		new ServerLoadBalancer().balance(servers,vms );
+	}
+
+	private Vm[] aVmsListEmpty() {
+		return new Vm[0];
+	}
+
+	private Server[] aServersListwith(Server ... servers) {
+		return servers;
+	}
+
+	private Server a(ServerBuilder builder) {
+		return builder.build();
+	}
+
+	private ServerBuilder server() {
+		return new ServerBuilder();
+	}
 
 }
