342a16934d4576164bce37b2ab20d39d44afbace
ciupa.kamil <194928@edu.p.lodz.pl>
2017-06-18 17:28:38 +0200

    test 5 refactor green

--- src/main/java/edu/iis/mto/serverloadbalancer/ServerLoadBalancer.java
+++ src/main/java/edu/iis/mto/serverloadbalancer/ServerLoadBalancer.java
@@ -5,15 +5,23 @@ public class ServerLoadBalancer {
 	public void balance(Server[] servers, Vm[] vms) {
 				
 		for (Vm vm : vms) {
-			Server lessLoadedServer = null;
-			
-			for (Server server : servers) {
-				if(lessLoadedServer == null || server.currentLoadPercentage < lessLoadedServer.currentLoadPercentage){
-					lessLoadedServer = server;
-				}
-			}
-			lessLoadedServer.addVm(vm);
+			addToLessloadedServer(servers, vm);
 		}
+	}
+
+	private void addToLessloadedServer(Server[] servers, Vm vm) {
+		Server lessLoadedServer = findLessLoadedServer(servers);
+		lessLoadedServer.addVm(vm);
+	}
+
+	private Server findLessLoadedServer(Server[] servers) {
+		Server lessLoadedServer = null;
+		for (Server server : servers) {
+			if(lessLoadedServer == null || server.currentLoadPercentage < lessLoadedServer.currentLoadPercentage){
+				lessLoadedServer = server;
+			}
+		}
+		return lessLoadedServer;
 	}
 
 }
